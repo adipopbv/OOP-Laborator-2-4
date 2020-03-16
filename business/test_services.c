384752596ea8ff3312cb4514c048a57e6037f2a6
Adrian Pop <adi.pop.bv@gmail.com>
2020-03-16 12:05:56 +0200

    Updated tests for dynamic allocation implementation

--- business/test_services.c
+++ business/test_services.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <assert.h>
 #include "../domain/exceptions.h"
@@ -16,7 +17,9 @@ void test_AddStock()
 	assert(strcmp(AddStock(&list, stock).message, e.message) == 0);
 	assert(strcmp(list.values[0].name, stock.name) == 0);
 	assert(strcmp(AddStock(&list, stock2).message, e.message) != 0);
-	assert(strcmp(list.values[1].name, stock2.name) != 0);
+	assert(list.currentLength == 1);
+	DeconstructList(&list);
+	DeconstructStock(&stock2);
 }
 
 void test_ModifyStock()
@@ -29,8 +32,11 @@ void test_ModifyStock()
 	Append(&list, stock);
 	assert(strcmp(ModifyStock(&list, stock, stock3).message, e.message) != 0);
 	assert(strcmp(list.values[0].name, stock3.name) != 0);
+	DeconstructStock(&stock3);
 	assert(strcmp(ModifyStock(&list, stock, stock2).message, e.message) == 0);
 	assert(strcmp(list.values[0].name, stock2.name) == 0);
+	DeconstructStock(&stock);
+	DeconstructList(&list);
 }
 
 void test_DeleteStock()
@@ -42,81 +48,81 @@ void test_DeleteStock()
 	Exception e = MakeException("");
 	Append(&list, stock);
 	assert(strcmp(DeleteStock(&list, stock).message, e.message) == 0);
-	assert(strcmp(list.values[list.length].name, stock.name) == 0);
+	assert(strcmp(list.values[list.currentLength].name, stock.name) == 0);
+	DeconstructStock(&stock);
 	Append(&list, stock2);
 	Append(&list, stock3);
 	assert(strcmp(DeleteStock(&list, stock2).message, e.message) == 0);
 	assert(strcmp(list.values[0].name, stock2.name) != 0);
+	DeconstructList(&list);
+	DeconstructStock(&stock2);
 }
 
 void test_NameStartsWithLetter()
 {
 	List list = MakeList();
-	List list2 = MakeList();
 	Stock stock = MakeStock("ceva", "cineva", 12);
 	Stock stock2 = MakeStock("ceva", "cineva", 12);
 	Stock stock3 = MakeStock("deva", "cineva", 12);
 	Append(&list, stock);
 	Append(&list, stock2);
 	Append(&list, stock3);
-	Append(&list2, stock);
-	Append(&list2, stock2);
-	for (int i=0; i<2; i++)
-		assert(strcmp(NameStartsWithLetter(&list, 'c').values[i].name, list2.values[i].name) == 0);
+	List temp = NameStartsWithLetter(&list, 'c');
+	assert(strcmp(temp.values[0].name, stock.name) == 0);
+	assert(strcmp(temp.values[1].name, stock2.name) == 0);
+	DeconstructList(&list);
+	free(temp.values);
 }
 
 void test_MaximumQuantity()
 {
 	List list = MakeList();
-	List list2 = MakeList();
 	Stock stock = MakeStock("ceva", "cineva", 12);
 	Stock stock2 = MakeStock("ceva", "cineva", 22);
 	Stock stock3 = MakeStock("deva", "cineva", 32);
 	Append(&list, stock);
 	Append(&list, stock2);
 	Append(&list, stock3);
-	Append(&list2, stock);
-	assert(strcmp(MaximumQuantity(&list, 20).values[0].name, list2.values[0].name) == 0);
+	List temp = MaximumQuantity(&list, 20);
+	assert(strcmp(temp.values[0].name, stock.name) == 0);
+	DeconstructList(&list);
+	free(temp.values);
 }
 
 void test_OrderByName()
 {
 	List list = MakeList();
-	List list2 = MakeList();
 	Stock stock = MakeStock("beva", "cineva", 12);
 	Stock stock2 = MakeStock("ceva", "cineva", 22);
 	Stock stock3 = MakeStock("deva", "cineva", 32);
 	Append(&list, stock);
 	Append(&list, stock2);
 	Append(&list, stock3);
-	Append(&list2, stock3);
-	Append(&list2, stock2);
-	Append(&list2, stock);
-	for (int i=0; i<3; i++)
-	{
-		assert(strcmp(OrderByName(&list, 1).values[i].name, list.values[i].name) == 0);
-		assert(strcmp(OrderByName(&list, -1).values[i].name, list2.values[i].name) == 0);
-	}
+	assert(strcmp(OrderByName(&list, 1).values[0].name, stock.name) == 0);
+	assert(strcmp(OrderByName(&list, 1).values[1].name, stock2.name) == 0);
+	assert(strcmp(OrderByName(&list, 1).values[2].name, stock3.name) == 0);
+	assert(strcmp(OrderByName(&list, -1).values[0].name, stock3.name) == 0);
+	assert(strcmp(OrderByName(&list, -1).values[1].name, stock2.name) == 0);
+	assert(strcmp(OrderByName(&list, -1).values[2].name, stock.name) == 0);
+	DeconstructList(&list);
 }
 
 void test_OrderByQuantity()
 {
 	List list = MakeList();
-	List list2 = MakeList();
 	Stock stock = MakeStock("beva", "cineva", 12);
 	Stock stock2 = MakeStock("ceva", "cineva", 22);
 	Stock stock3 = MakeStock("deva", "cineva", 32);
 	Append(&list, stock);
 	Append(&list, stock2);
 	Append(&list, stock3);
-	Append(&list2, stock3);
-	Append(&list2, stock2);
-	Append(&list2, stock);
-	for (int i=0; i<3; i++)
-	{
-		assert(strcmp(OrderByQuantity(&list, 1).values[i].name, list.values[i].name) == 0);
-		assert(strcmp(OrderByQuantity(&list, -1).values[i].name, list2.values[i].name) == 0);
-	}
+	assert(strcmp(OrderByQuantity(&list, 1).values[0].name, stock.name) == 0);
+	assert(strcmp(OrderByQuantity(&list, 1).values[1].name, stock2.name) == 0);
+	assert(strcmp(OrderByQuantity(&list, 1).values[2].name, stock3.name) == 0);
+	assert(strcmp(OrderByQuantity(&list, -1).values[0].name, stock3.name) == 0);
+	assert(strcmp(OrderByQuantity(&list, -1).values[1].name, stock2.name) == 0);
+	assert(strcmp(OrderByQuantity(&list, -1).values[2].name, stock.name) == 0);
+	DeconstructList(&list);
 }
 
 void RunAllServiceTests()
